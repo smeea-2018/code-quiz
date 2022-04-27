f585a2d02b4075c04f1c0639eff869d9634ac587
smeea-2018 <smeeaarshad@gmail.com>
2022-04-27 17:21:01 +0100

    rendered question in javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,34 @@
+const question =
+  "Which of the following keywords is used to define a variable in Javascript?";
+
+const answer = ["var", "let", "Both var and let", "none"];
 const startButton = document.getElementById("start-quiz-button");
+const main = document.getElementById("main");
 
 const startQuiz = () => {
-  console.log("Welcome!");
+  //Remove start quiz section
+  const sectionStart = document.getElementById("start-quiz-id");
+  sectionStart.remove();
+  //Start Timer
+  //Render the first question
+  const section = document.createElement("section");
+  section.setAttribute("class", "question-section");
+  section.setAttribute("correct-answer", "Both var and let");
+
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "subtitle");
+  h2.textContent = question;
+
+  const ul = document.createElement("ul");
+  for (let i = 0; i < answer.length; i += 1) {
+    const li = document.createElement("li");
+    li.setAttribute("data-option", answer[i]);
+    li.textContent = answer[i];
+    ul.appendChild(li);
+  }
+
+  section.append(h2, ul);
+  main.append(section);
 };
 
 startButton.addEventListener("click", startQuiz);
